980ce7d62d2238d9a71a3cf5d2287aaf711ec1ea
Administrator <zr2013123@sina.com>
2019-08-19 16:10:57 +0800

    file

---
+++ Application/src/main/java/com/cmcm/greendamexplorer/dao/test/TestFavorite.java
@@ -0,0 +1,21 @@
+package com.cmcm.greendamexplorer.dao.test;
+
+import com.cmcm.greendamexplorer.core.engine.DeploymentOperation;
+import com.cmcm.greendamexplorer.dao.DaoFactory;
+import com.cmcm.greendamexplorer.dao.impl.FavoriteDao;
+import com.cmcm.greendamexplorer.entity.Favorite;
+
+public class TestFavorite  {
+
+    DeploymentOperation app = new DeploymentOperation();
+
+    public void testFindByPath() {
+
+        FavoriteDao dao = DaoFactory.getFavoriteDao(app.getApplicationContext());
+
+        Favorite favorite = dao.findFavoriteByFullPath("/sdcard/360/a.txt");
+        System.out.println(favorite);
+
+    }
+
+}
